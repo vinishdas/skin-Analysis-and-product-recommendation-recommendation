8c9ba3bba2e0f81fb5f0f267d07860c7020b7869
ahad <ahadasif.cs22@sahyadri.edu.in>
2024-12-21 20:37:42 +0530

    About section done

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -4,14 +4,24 @@ import "./style/About.css";
 // Sample data for team members
 const teamMembers = [
   {
-    name: "Mohammed Nahil Nasir",
-    github: "https://github.com/johndoe",
-    email: "johndoe@example.com",
+    name: " Nahil Nasir",
+    github: "https://github.com/nahilnasir",
+    email: "nahil.ai22@sahyadri.edu.in",
   },
   {
     name: "Tapan KR",
-    github: "https://github.com/janesmith",
+    github: "https://github.com/Tapan003",
+    email: "tapan.cs22@sahaydri.edu.in",
+  },
+  {
+    name: "Ahad Asif Hussain",
+    github: "https://github.com/ahadhuss3in",
     email: "janesmith@example.com",
+  },
+  {
+    name: "A Vinish Das",
+    github: "https://github.com/vinishdas",
+    email: "vinish.cs22@sahyadri.edu.in",
   },
   // Add more team members here
 ];
@@ -19,8 +29,10 @@ const teamMembers = [
 const About = () => {
   return (
     <div className="page">
+      <h1>About Our SkinCare App</h1>
     <div className="about">
-      <h1>About This App</h1>
+      
+      
       <p>
         This project, developed by a third-year student at Sahyadri College,
         utilizes Convolutional Neural Networks (CNNs) for skin care analysis.
@@ -29,8 +41,9 @@ const About = () => {
         By leveraging deep learning techniques, the project automates the process of analyzing skin health,
         offering potential applications in personalized skin care recommendations.
         The model is trained on a dataset of skin images and is designed to provide accurate predictions,
-        enabling users to monitor and improve their skin health with the help of advanced 
+        enabling users to monitor and improve their skin health with the help of advanced technology.
       </p>
+      </div>
 
       <h2>Meet The Team</h2>
       <div className="team">
@@ -49,7 +62,7 @@ const About = () => {
         ))}
       </div>
     </div>
-    </div>
+    
   );
 };
 
